4adb7905acec97b195e12b2e0dc0bcb76914e236
a2v <81217105+hikkazdr@users.noreply.github.com>
2023-04-10 01:33:11 +0300

    2.4

--- sandbox/src/main/java/ru/stqa/javaCourse/sandbox/Exercise2.java
+++ sandbox/src/main/java/ru/stqa/javaCourse/sandbox/Exercise2.java
@@ -1,20 +1,9 @@
 package ru.stqa.javaCourse.sandbox;
 
-import static java.lang.String.format;
-
 public class Exercise2 {
 
     public static void main(String[] args) {
-        Point p1 = new Point(-1.0, 4.0);
-        Point p2 = new Point(6.0, -2.0);
-        System.out.println(distance(p1, p2));
-    }
-    
-    public static String distance(Point p1, Point p2){
-        double distanceX = p2.x - p1.x;
-        double distanceY = p2.y - p1.y;
-        double distance =  Math.sqrt((distanceX*distanceX) + (distanceY*distanceY));
-        return format("Расстояние между точкой на координате (%s;%s) и точкой (%s;%s) = %f",
-                p1.x, p1.y, p2.x, p2.y, distance);
+        Point allPoints = new Point(-1.0, 4.0, 6.0, -2.0);
+        System.out.println(allPoints.distance());
     }
 }
