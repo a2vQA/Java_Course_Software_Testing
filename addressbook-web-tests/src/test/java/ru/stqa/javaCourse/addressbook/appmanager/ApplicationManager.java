f5b43d92328e6e6f0435ad1e5927ef2d41092808
a2v <81217105+a2vQA@users.noreply.github.com>
2023-04-27 00:15:21 +0300

    Added possibility to run tests in diff browsers

--- addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/appmanager/ApplicationManager.java
@@ -2,16 +2,30 @@ package ru.stqa.javaCourse.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 
 public class ApplicationManager {
+    private final String browser;
     private ContactHelper contactHelper;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
     public WebDriver wd;
 
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
     public void init() {
-        wd = new ChromeDriver();
+        if (browser.equals(Browser.CHROME.browserName())) {
+            wd = new ChromeDriver();
+        } else if (browser.equals(Browser.FIREFOX.browserName())){
+            wd = new FirefoxDriver();
+        } else if (browser.equals(Browser.EDGE.browserName())){
+            wd = new EdgeDriver();
+        }
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
