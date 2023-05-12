e94e159ecb3a16f9b75aee72af043bcb5a40d342
a2v <81217105+a2vQA@users.noreply.github.com>
2023-05-13 01:41:47 +0300

    Added verification for ObjectList in GroupCreation

--- addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/tests/GroupCreationTests.java
@@ -22,13 +22,7 @@ public class GroupCreationTests extends BaseTest {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (GroupData g : after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-        groupData.setId(max);
+        groupData.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(groupData);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
