0f27a32b676c6c01e11a61fd8c3226b6527db29b
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2023-06-06 22:04:01 +0300

    Fixed naming

--- addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/tests/DbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/tests/DbConnectionTest.java
@@ -6,7 +6,7 @@ import ru.stqa.javaCourse.addressbook.model.Groups;
 
 import java.sql.*;
 
-public class DbConnectionTests {
+public class DbConnectionTest {
 
     @Test
     public void testDbConnection() {
