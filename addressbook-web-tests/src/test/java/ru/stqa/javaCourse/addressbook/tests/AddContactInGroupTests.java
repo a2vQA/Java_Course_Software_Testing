cc47fe58bc65c70f4b2fb22d15e760290319bde6
Artyomenko Vladislav <81217105+a2vQA@users.noreply.github.com>
2023-06-08 10:51:57 +0300

    WIP #16

--- addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/tests/AddContactInGroupTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCourse/addressbook/tests/AddContactInGroupTests.java
@@ -3,7 +3,6 @@ package ru.stqa.javaCourse.addressbook.tests;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.javaCourse.addressbook.model.ContactData;
-import ru.stqa.javaCourse.addressbook.model.GroupData;
 
 public class AddContactInGroupTests extends BaseTest {
 
@@ -13,12 +12,12 @@ public class AddContactInGroupTests extends BaseTest {
                 app.goTo().homePage();
                 app.contact().initContactCreation();
                 app.contact().createContactWithoutGroup(new ContactData().withFirstName("Vladislav").withLastName("Artyomenko").withAddress("Moscow")
-                        .withMobilePhone("+79999999999").withPrimaryEmail("javaCourse@test.ru"));
+                        .withMobilePhone("+79999999999").withHomePhone("999999").withWorkPhone("888888")
+                        .withPrimaryEmail("javaCourse@test.ru").withSecondaryEmail("javaCourse2@test.ru").withThirdEmail("javaCourse3@test.ru"));
         }
-        app.goTo().groupPage();
-        app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
-        GroupData group = app.db().groups().iterator().next();
-        int newGroupId = group.getId();
+//        app.goTo().groupPage();
+//        app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
+//        GroupData group = app.db().groups().iterator().next();
     }
 
     @Test
